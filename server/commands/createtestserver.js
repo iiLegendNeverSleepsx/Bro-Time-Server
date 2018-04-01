f745eebf8d9abd3446cb008729fc79f5af426148
Yikey <gtc122004@gmail.com>
2018-04-01 17:02:55 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -8,7 +8,7 @@ module.exports = {
 	execute: (call) => {
 		if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
-		var realGuild = call.client.guilds.get("330913265573953536");
+		var realGuild = call.client.guilds.get("330913265573953536")
 		realGuild.channels.forEach(async function(channel) {
 			await testGuild.createChannel(reverse(channel.name), channel.type, channel.positionOverwrites).then(newChannel => {
 				newChannel.setTopic(channel.topic);

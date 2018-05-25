b8ef307f5dab5e647cda4f609f39a0f297961d7a
Yikey <gtc122004@gmail.com>
2018-05-24 22:52:05 -0400

    fix garbage

--- kitchen order.js
+++ kitchen order.js
@@ -1,48 +1,42 @@
-const fsn = require('fs-nextra');
-exports.run = (client, msg, args) => {
-    order = args.join(' ')
-    fsn.readJSON('./orders.json')
-        .then((orderDB) => {
-            function makeID() {
-                let id = '';
-                let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
-                for (let i = 0; i < 5; i++) id += possible.charAt(Math.floor(Math.random() * possible.length));
-                //eslint-disable-next-line newline-before-return
-                return id;
-            }
-            const orderID = makeID()
+const fsn = require("fs-nextra");
+module.exports = {
+	id: "order",
+	execute: (call) => {
+		const order = call.params.readRaw();
+		fsn.readJSON("./orders.json").then((orderDB) => {
+			function makeID() {
+				var id = "";
+				var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
+				for (var i = 0; i < 5; i++) id += possible.charAt(Math.floor(Math.random() * possible.length));
+				//eslint-disable-next-line newline-before-return
+				return id;
+			}
+			const orderID = makeID();
 
-            if (!orderDB[orderID]) orderDB[orderID] = {
-                'orderID': orderID,
-                'userID': msg.author.id,
-                'guildID': msg.guild.id,
-                'channelID': msg.channel.id,
-                'order': order,
-                'status': 'Waiting'
-            }
+			if (!orderDB[orderID]) orderDB[orderID] = {
+				"orderID": orderID,
+				"userID": call.message.author.id,
+				"guildID": call.message.guild.id,
+				"channelID": call.message.channel.id,
+				"order": order,
+				"status": "Waiting"
+			};
 
-            fsn.writeJSON('./orders.json', orderDB, {
-                    replacer: null,
-                    spaces: 2
-                })
-                .then(() => {
-                    msg.reply(`Your order has been sent to the kitchen! Your order ID is \`${orderID}\``)
-                    let ordersChan = client.channels.get('433831764105101332')
-                    ordersChan.send(`__**Order**__
-**OrderID:** ${orderID}
-**Order:** ${order}
-**Customer:** ${msg.author.tag} (${msg.author.id})
-**Ordered from:** #${msg.channel.name} (${msg.channel.id}) in ${msg.guild.name} (${msg.guild.id})
-**Status:** Awaiting a cook`)
-                })
-                .catch((err) => {
-                    if (err) {
-                        msg.reply(`There was a database error!
-Show the following message to a developer:
-\`\`\`${err}\`\`\``)
-                        console.error(`Error in order ${orderID}
-${err}`)
-                    }
-                })
-        })
-}
+			fsn.writeJSON("./orders.json", orderDB, { replacer: null, spaces: 2 }).then(() => {
+				call.message.reply(`Your order has been sent to the kitchen! Your order ID is \`${orderID}\``);
+				var ordersChan = call.client.channels.get("433831764105101332");
+				ordersChan.send("__**Order**__\n" +
+					`**OrderID:** ${orderID}\n` +
+					`**Order:** ${order}\n`
+					`**Customer:** ${call.message.author.tag} (${call.message.author.id})\n`
+					`**Ordered from:** #${call.message.channel.name} (${call.message.channel.id}) in ${call.message.guild.name} (${call.message.guild.id})\n`
+					`**Status:** Awaiting a cook\n`);
+			}).catch((err) => {
+				if (err) {
+					call.message.reply(`There was a database error! Show the following message to a developer: \`\`\`${err}\`\`\``)
+					console.error(`Error in order ${orderID} ${err}`);
+				}
+			});
+		});
+	}
+};
